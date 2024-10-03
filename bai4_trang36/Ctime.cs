bc2db99daba2ac1f6953726f4a9595bb98b94a2f
TS0906 <luonghuuthinh377@gmail.com>
2024-10-03 13:52:18 +0700

    update

---
+++ bai4_trang36/Ctime.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai4_trang36
+{
+    public class CTime
+    {
+        private int gio;
+        private int phut;
+        private int giay;
+
+        public CTime()
+        {
+            gio = 0;
+            phut = 0;
+            giay = 0;
+        }
+        public CTime(int a, int b, int c)
+        {
+            this.gio = a;
+            this.phut = b;
+            this.giay = c;
+            ChuanHoaThoiGian();
+        }
+
+        private void ChuanHoaThoiGian()
+        {
+            if (giay >= 60)
+            {
+                phut += giay / 60;
+                giay %= 60;
+            }
+            if (phut >= 60)
+            {
+                gio += phut / 60;
+                phut %= 60;
+            }
+            if (giay < 0)
+            {
+                giay += 60;
+                phut--;
+            }
+            if (phut < 0)
+            {
+                phut += 60;
+                gio--;
+            }
+        }
+
+        public void NhapThoiGian(int a, int b, int c)
+        {
+            this.gio = a;
+            this.phut = b;
+            this.giay = c;
+            ChuanHoaThoiGian();
+        }
+
+        public string XuatThoiGian()
+        {
+            return $"{gio:D2}:{phut:D2}:{giay:D2}"; //D2 dung de them so 0 vao truoc nhung chu so co 1 chu so
+        }
+
+        public static CTime operator +(CTime time, int giayThem)
+        {
+            CTime kq = new CTime(time.gio, time.phut, time.giay + giayThem);
+            kq.ChuanHoaThoiGian();
+            return kq;
+        }
+
+  
+        public static CTime operator -(CTime time, int giayBot)
+        {
+            CTime kq = new CTime(time.gio, time.phut, time.giay - giayBot);
+            kq.ChuanHoaThoiGian();
+            return kq;
+        }
+
+        public static CTime operator ++(CTime time)
+        {
+            CTime kq = new CTime(time.gio, time.phut, time.giay + 1);
+            kq.ChuanHoaThoiGian();
+            return kq;
+        }
+
+        public static CTime operator --(CTime time)
+        {
+            CTime kq = new CTime(time.gio, time.phut, time.giay - 1);
+            kq.ChuanHoaThoiGian();
+            return kq;
+        }
+    }
+}
---
+++ bai4_trang36/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai4_trang36
+{
+    public partial class Form1 : Form
+    {
+        private CTime thoiGian;
+        public Form1()
+        {
+            InitializeComponent();
+            thoiGian = new CTime();
+        }
+
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            int gio = int.Parse(txtGio.Text);
+            int phut = int.Parse(txtPhut.Text);
+            int giay = int.Parse(txtGiay.Text);
+
+            thoiGian.NhapThoiGian(gio, phut, giay);
+            int giaynew = int.Parse(txtGiaynew.Text);
+            txtThoigian.Text = thoiGian.XuatThoiGian();
+        }
+        private void butCong_Click(object sender, EventArgs e)
+        {
+            int giayThem = int.Parse(txtGiaynew.Text);
+            thoiGian = thoiGian + giayThem;
+            txtKq.Text = thoiGian.XuatThoiGian();
+        }
+        private void butTru_Click(object sender, EventArgs e)
+        {
+            int giayThem = int.Parse(txtGiaynew.Text);
+            thoiGian = thoiGian - giayThem;
+            txtKq.Text = thoiGian.XuatThoiGian();
+        }
+        private void butCong1_Click(object sender, EventArgs e)
+        {
+            thoiGian++;
+            txtKq.Text = thoiGian.XuatThoiGian();
+        }
+        private void butTru1_Click(object sender, EventArgs e)
+        {
+            thoiGian--;
+            txtKq.Text = thoiGian.XuatThoiGian();
+        }
+    }
+}
